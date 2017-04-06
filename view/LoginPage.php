8266bd3036d9fdcef6715ca442d66dd57d73f717
rogerk <rog_kol03@hotmail.com>
2017-04-06 22:28:20 +0200

    fff

--- view/LoginPage.php
+++ view/LoginPage.php
@@ -54,11 +54,9 @@ $error = $GLOBALS["errorMessage"];
             
         </div>
         <script type="text/javascript" src="system/js/hide-show-password.js"></script>
-        <script type="text/javascript" src="system/js/userAdm.js"></script>
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="../Bootstrap/js/bootstrap.min.js"></script>
     </body>
 </html>
 
