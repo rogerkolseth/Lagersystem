6d5552327e0201aa0f011517e4eb7a62c7a0d633
rogerk <rog_kol03@hotmail.com>
2017-04-06 21:26:37 +0200

    ds

--- view/LoginPage.php
+++ view/LoginPage.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 
 <?php
 if (isset($GLOBALS["errorMessage"])){
@@ -20,7 +14,7 @@ $error = $GLOBALS["errorMessage"];
 
     </head>
     <body>
-        
+        <p> hei</p>
         
             <div class="container">
             
