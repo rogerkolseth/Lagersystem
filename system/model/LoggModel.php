6f60539b43db485657ab9de5957840b296b4c8f9
rogerk <rog_kol03@hotmail.com>
2017-04-14 19:15:32 +0200

    test

--- system/model/LoggModel.php
+++ system/model/LoggModel.php
@@ -7,16 +7,16 @@ class LoggModel {
     const TABLE = "logg";
     const CHECK_TABLE = "loggtype";
     
-    const SELECT_QUERY = 
-         "SELECT lt.typeName, l.desc, s1.storageName, s2.storageName AS fromStorage, s3.storageName AS toStorage, l.quantity, l.oldQuantity, l.newQuantity, l.differential, u1.username, u2.username AS onUsername, p.productName, l.customerNr, DATE_FORMAT(l.date,'%d %b %Y %T') AS date FROM " . LoggModel::TABLE . " AS l "
-        ."LEFT JOIN storage as s1 ON l.storageID = s1.storageID "
-        ."LEFT JOIN storage as s2 ON l.fromStorageID = s2.storageID "
-        ."LEFT JOIN storage as s3 ON l.toStorageID = s3.storageID "
-        ."LEFT JOIN users as u1 ON l.userID = u1.userID "
-        ."LEFT JOIN users as u2 ON l.onUserID = u2.userID "
-        ."LEFT JOIN loggType as lt ON l.typeID = lt.typeID "    
-        ."LEFT JOIN products as p ON l.productID = p.productID WHERE lt.typeName LIKE :givenSearchWord OR l.desc LIKE :givenSearchWord OR s1.storageName LIKE :givenSearchWord OR s2.storageName LIKE :givenSearchWord OR s3.storageName LIKE :givenSearchWord OR l.quantity LIKE :givenSearchWord "
-            . "OR l.oldQuantity LIKE :givenSearchWord OR l.newQuantity LIKE :givenSearchWord OR l.differential LIKE :givenSearchWord OR u1.username LIKE :givenSearchWord OR u2.username OR p.productName LIKE :givenSearchWord OR customerNr LIKE :givenSearchWord ORDER BY date DESC LIMIT 100";
+    const SELECT_QUERY = "SELECT * FROM ". LoggModel::TABLE; 
+   //      "SELECT lt.typeName, l.desc, s1.storageName, s2.storageName AS fromStorage, s3.storageName AS toStorage, l.quantity, l.oldQuantity, l.newQuantity, l.differential, u1.username, u2.username AS onUsername, p.productName, l.customerNr, DATE_FORMAT(l.date,'%d %b %Y %T') AS date FROM " . LoggModel::TABLE . " AS l "
+   //     ."LEFT JOIN storage as s1 ON l.storageID = s1.storageID "
+   //     ."LEFT JOIN storage as s2 ON l.fromStorageID = s2.storageID "
+   //     ."LEFT JOIN storage as s3 ON l.toStorageID = s3.storageID "
+   //     ."LEFT JOIN users as u1 ON l.userID = u1.userID "
+   //     ."LEFT JOIN users as u2 ON l.onUserID = u2.userID "
+   //     ."LEFT JOIN loggType as lt ON l.typeID = lt.typeID "    
+   //     ."LEFT JOIN products as p ON l.productID = p.productID WHERE lt.typeName LIKE :givenSearchWord OR l.desc LIKE :givenSearchWord OR s1.storageName LIKE :givenSearchWord OR s2.storageName LIKE :givenSearchWord OR s3.storageName LIKE :givenSearchWord OR l.quantity LIKE :givenSearchWord "
+   //         . "OR l.oldQuantity LIKE :givenSearchWord OR l.newQuantity LIKE :givenSearchWord OR l.differential LIKE :givenSearchWord OR u1.username LIKE :givenSearchWord OR u2.username OR p.productName LIKE :givenSearchWord OR customerNr LIKE :givenSearchWord ORDER BY date DESC LIMIT 100";
     
     const SELECT_LATEST_QUERY = 
          "SELECT lt.typeName, l.desc, s1.storageName, s2.storageName AS fromStorage, s3.storageName AS toStorage, l.quantity, l.oldQuantity, l.newQuantity, l.differential, u1.username, u2.username AS onUsername, p.productName, l.customerNr, DATE_FORMAT(l.date,'%d %b %Y %T') AS date FROM " . LoggModel::TABLE . " AS l "
