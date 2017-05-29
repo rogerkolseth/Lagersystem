b9e18311b84b8fb03ff1a806cd3813cdd82be577
rogerk <rog_kol03@hotmail.com>
2017-05-29 23:20:51 +0200

    meir

--- system/js/hide-show-password.js
+++ system/js/hide-show-password.js
@@ -1,42 +1,32 @@
-(function ()
-{
-    var PasswordToggler = function (element, field)
-    {
+(function (){
+    var PasswordToggler = function (element, field){
         this.element = element;
         this.field = field;
-
         this.toggle();
     };
 
-    PasswordToggler.prototype =
-            {
-                toggle: function ()
-                {
+    PasswordToggler.prototype = {
+                toggle: function (){
                     var self = this;
                     self.element.addEventListener("change", function ()
                     {
-                        if (self.element.checked)
-                        {
+                        if (self.element.checked) {
                             self.field.setAttribute("type", "text");
-                        } else
-                        {
+                        } else {
                             self.field.setAttribute("type", "password");
                         }
                     }, false);
                 }
             };
             
-            document.addEventListener("DOMContentLoaded", function()
-            {
+            document.addEventListener("DOMContentLoaded", function(){
                 var checkbox = document.querySelector("#show-hide"),
                 psw = document.querySelector("#psw"),
                 form = document.querySelector("#login");
                 
-                form.addEventListener("submit", function(e)
-                {
+                form.addEventListener("submit", function(e) {
                     e.prevendDefault();
                 }, false);
-                
                 var toggler = new PasswordToggler(checkbox, psw);
             });
 
