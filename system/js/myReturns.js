4261fc2a0af16ae3f606b9851f10502fbc0b143a
rogerk <rog_kol03@hotmail.com>
2017-05-30 17:16:22 +0200

    kommentar

--- system/js/myReturns.js
+++ system/js/myReturns.js
@@ -1,62 +1,72 @@
 
-$(function () {
+/**
+ * Get all returns from logged in user
+ */
+$(function getMyReturns() {
 
     $.ajax({
         type: 'GET',
-        url: '?request=getMyReturns',
+        url: '?request=getMyReturns',   // request given to controller
         dataType: 'json',
         success: function (data) {
-            myReturnsTemplate(data);
-            userReturnTemplate(data);
+            myReturnsTemplate(data);    // display returns
+            userReturnTemplate(data);   // get usernames for advance serach
         }
     });
 });
 
 
 
-// Update return information -->
 
+/**
+ * update return table
+ */
 function UpdateReturnsTable() {
     $(function () {
         $.ajax({
             type: 'GET',
-            url: '?request=getMyReturns',
+            url: '?request=getMyReturns',   // request given to controller
             dataType: 'json',
             success: function (data) {
-                myReturnsTemplate(data);
-                userReturnTemplate(data);
-            }
+                myReturnsTemplate(data);    // display returns
+                userReturnTemplate(data);   // get usernames for advance serach
+            }   
         });
     });
 }
 
 
-
+/**
+ * display all returns
+ * takes given data and poplate template
+ */
 function myReturnsTemplate(data) {
+    // handle bar helper, check if a value equals another value
     Handlebars.registerHelper('if_eq', function (a, b, opts) {
         if (a == b)
             return opts.fn(this);
         else
             return opts.inverse(this);
     });
+    //takes template and populate it with passed array
     var rawTemplate = document.getElementById("myReturnsTemplate").innerHTML;
     var compiledTemplate = Handlebars.compile(rawTemplate);
     var mySalesnGeneratedHTML = compiledTemplate(data);
-
+    // display template in choosen ID tag
     var myReturnsContainer = document.getElementById("myReturnsContainer");
     myReturnsContainer.innerHTML = mySalesnGeneratedHTML;
 
 }
 
 
-// SEARCH FOR RETURNS -->
-
-
-$(function POSTsearchForReturn() {
-
+/**
+ * Search for return
+ */
+$(function searchForReturn() {
+    // run if form is submitted
     $('#searchForReturns').submit(function () {
-        var url = $(this).attr('action');
-        var data = $(this).serialize();
+        var url = $(this).attr('action');   // get action from form
+        var data = $(this).serialize();     // serialize data in form
 
         $.ajax({
             type: 'POST',
@@ -64,8 +74,8 @@ $(function POSTsearchForReturn() {
             data: data,
             dataType: 'json',
             success: function (data) {
-                $("#searchForReturns")[0].reset();
-                myReturnsTemplate(data);
+                $("#searchForReturns")[0].reset();  // reset form
+                myReturnsTemplate(data);        // update return tabel
             }
         });
         return false;
@@ -74,19 +84,21 @@ $(function POSTsearchForReturn() {
 
 
 
-
+/**
+ * edit a given return
+ */
 $(function editMyReturns() {
+    //check if editReturns button inside myReturnsContainer is clicked
     $('#myReturnsContainer').delegate('.editReturns', 'click', function () {
-
-        var givenReturnsID = $(this).attr('data-id');
+        var givenReturnsID = $(this).attr('data-id');   // get data-id from button
         $.ajax({
             type: 'POST',
-            url: '?request=getReturnsFromID',
-            data: {givenReturnsID: givenReturnsID},
+            url: '?request=getReturnsFromID',       // request given to controller
+            data: {givenReturnsID: givenReturnsID},     // data posted to controller
             dataType: 'json',
             success: function (data) {
-                editReturnsTemplate(data);
-                $('#editReturnsModal').modal('show');
+                editReturnsTemplate(data);      // display info from choosen return
+                $('#editReturnsModal').modal('show');   // show edit return modal
             }
         });
         return false;
@@ -94,19 +106,23 @@ $(function editMyReturns() {
     });
 });
 
+/**
+ * Display registered mac in a return
+ */
 $(function showMacReturns() {
+    //check if showMac button inside myReturnsContainer is clicked
     $('#myReturnsContainer').delegate('.showMac', 'click', function () {
-        var givenReturnsID = $(this).attr('data-id');
+        var givenReturnsID = $(this).attr('data-id');   // get data-id from button
         $.ajax({
             type: 'POST',
-            url: '?request=getReturnsMacFromID',
-            data: {givenReturnsID: givenReturnsID},
+            url: '?request=getReturnsMacFromID',    // request given to controller
+            data: {givenReturnsID: givenReturnsID},     //data posted to controller
             dataType: 'json',
             success: function (data) {
-                $('#macReturnsModal').modal('show');
-                var $macReturnTemplate = $('#macReturnsContainer');
-                $macReturnTemplate.empty();             
-                $.each(data.myReturnsMac, function (i, item) {
+                $('#macReturnsModal').modal('show');    // show mac modal
+                var $macReturnTemplate = $('#macReturnsContainer'); // set element-id to populate
+                $macReturnTemplate.empty();             // empty element-id
+                $.each(data.myReturnsMac, function (i, item) {  // populate selected element-id with macadresses
                 $macReturnTemplate.append('<tr><td>'+ item.macAdresse+'</td></tr>');
                 });
             }
@@ -115,44 +131,51 @@ $(function showMacReturns() {
     });
 });
 
-
-// Display edit sale Template -->
-
+/**
+ * display edit return 
+ * takes given data and poplate template
+ */
 function editReturnsTemplate(data) {
+    //takes template and populate it with passed array
     var rawTemplate = document.getElementById("editReturnTemplate").innerHTML;
     var compiledTemplate = Handlebars.compile(rawTemplate);
     var editReturnGeneratedHTML = compiledTemplate(data);
-
+    // display template in choosen ID tag
     var returnContainer = document.getElementById("editReturnContainer");
     returnContainer.innerHTML = editReturnGeneratedHTML;
 }
 
 
-// POST results from editing, and updating the table-->
-
-$(function POSTeditReturnsInfo() {
-
+/**
+ * Save edited changes
+ */
+$(function updateReturnInfo() {
+    // run if edit return form is submitted
     $('#editReturn').submit(function () {
-        var url = $(this).attr('action');
-        var data = $(this).serialize();
+        var url = $(this).attr('action');   // get form action
+        var data = $(this).serialize();     // serialize form data
         $.ajax({
             type: 'POST',
             url: url,
             data: data,
             dataType: 'json',
             success: function () {
-                $('#editReturnsModal').modal('hide');
-                UpdateReturnsTable();
+                $('#editReturnsModal').modal('hide');   // hide edit return modal
+                UpdateReturnsTable();   // update return table
             }
         });
         return false;
     });
 });
 
-
+/**
+ * populate advance search for return 
+ */
 function userReturnTemplate(data) {
+    // set element-id to be populated, and empty it
     var $usernameTemplate = $('#chooseUserReturnContainer');
     $usernameTemplate.empty();
+    // populate element-id with all usernames in system, with an checkbox
     $usernameTemplate.append('<tr><td id="bordernone">Alle</td> <td id="bordernone"><input id="chooseUserSale" type="checkbox" name="username[]" value="0"></td></tr>');
     $.each(data.usernames, function (i, item) {
         $usernameTemplate.append('<tr><td id="bordernone">' + item.username + '</td> <td id="bordernone"><input id="chooseUserReturn" type="checkbox" name="username[]" value="' + item.userID + '"></td></tr>');
@@ -160,19 +183,22 @@ function userReturnTemplate(data) {
     $usernameTemplate.append('<div class="pull-right"> <input class="form-control btn btn-primary" type="submit" form="showUserReturn"  value="Velg"> </div>');
 }
 
-$(function POSTshowUserReturn() {
-
+/**
+ * Get selected users return
+ */
+$(function getUserReturn() {
+    // run if show user return form is submitted
     $('#showUserReturn').submit(function () {
-        var url = $(this).attr('action');
-        var data = $(this).serialize();
+        var url = $(this).attr('action');   // get form action
+        var data = $(this).serialize();     // serialize from data
         $.ajax({
             type: 'POST',
             url: url,
             data: data,
             dataType: 'json',
             success: function (data) {
-                myReturnsTemplate(data);
-                $('.dropdown.open').removeClass('open');
+                myReturnsTemplate(data);        // display result in return table
+                $('.dropdown.open').removeClass('open');       // close search dropdown 
             }
         });
         return false;
